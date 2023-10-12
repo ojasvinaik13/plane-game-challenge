a588e9b66306bf9feb875a6d38b75d1267e26f3e
ojasvinaik13 <ojasvi.naik@gmail.com>
2023-10-11 23:34:22 -0700

    Bug fixes

--- Assets/Scripts/PlayerControllerX.cs
+++ Assets/Scripts/PlayerControllerX.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public float speed;
-    public float rotationSpeed;
+    public float speed = 15.0f;
+    public float rotationSpeed = 10.0f;
     public float verticalInput;
-
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -19,11 +19,11 @@ public class PlayerControllerX : MonoBehaviour
     {
         // get the user's vertical input
         verticalInput = Input.GetAxis("Vertical");
-
+        
         // move the plane forward at a constant rate
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput);
+        transform.Rotate(Vector3.right * verticalInput * rotationSpeed * Time.deltaTime);
     }
 }
